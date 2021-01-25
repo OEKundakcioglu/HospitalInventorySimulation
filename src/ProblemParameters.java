8a8b720c315d0a187f50fc3e7b7d5a2acd9cfe92
Erhun Kundakcioglu <erhunk@gmail.com>
2021-01-24 22:56:52 -0500

    Update ProblemParameters.java

--- src/ProblemParameters.java
+++ src/ProblemParameters.java
@@ -7,26 +7,23 @@ public class ProblemParameters {
 
 	// demand and recovery parameters
 
-	//public double[][] annualDemandRateAlternatives = { { 530, 210, 94 }, { 940, 380, 9 }, { 5980, 440, 14 },
-	//		{ 760, 430 }, { 50, 60, 70, 150, 160, 170 }, { 50, 60, 70, 350, 360, 370 } };
-	public double[][] annualDemandRateAlternatives = { { 10, 50, 100, 500, 1000 } };
+	public double[][] annualDemandRateAlternatives = { { 530, 210, 94 }, { 940, 380, 9 }, { 5980, 440, 14 },
+			{ 760, 430 }, { 50, 60, 70, 150, 160, 170 }, { 50, 60, 70, 350, 360, 370 } };
 	public double[] annualDemandRates;
 	public String outFilePre = "results/output";
 	public String outFile = "";
 
-	public double[] recoveryRates = { 0.5, 20 };
+	public double[] recoveryRates = { 1,2,3,4,5,6 };
 
 	// inventory parameters to be plotted
-	public double[] totalStockinAnnualDemand = { 0.25, 1 };
+	public double[] totalStockinAnnualDemand = { 0.25, 0.5, 0,75, 1 };
 	public double[] fractionsofPooled = { 0, 0.25, 0.5, 0.75, 1 };
 
 	// sensitivity parameters to analyze deviation from optimal levels to transfer
 	// from pooled to reserved
-	//public double[] poolSensitivityParameters = { 0.1, 0.2, 0.3 };
-	//public double[] thresholdSensitivityParameters = { 0.1, 0.2, 0.3 };
-	public double[] poolSensitivityParameters = {  };
-	public double[] thresholdSensitivityParameters = {  };
-
+	public double[] poolSensitivityParameters = { 0.1, 0.2, 0.3 };
+	public double[] thresholdSensitivityParameters = { 0.1, 0.2, 0.3 };
+	
 	public void setProblemParameters(int i) {
 		this.annualDemandRates = this.annualDemandRateAlternatives[i];
 		this.outFile = outFilePre + (i + 1) + ".csv";
